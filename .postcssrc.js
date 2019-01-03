243c1540a5edac530afe016cb50cba36bdfe722d
sxxlearn2rock <sxx_uestc@163.com>
2019-01-03 17:35:26 +0800

    add rucksack

--- .postcssrc.js
+++ .postcssrc.js
@@ -2,11 +2,13 @@ module.exports = {
   plugins: {
     'postcss-import': {},
     // 'precss': {},
+    'rucksack-css': {},
     'postcss-preset-env': {},
     // mixins 要在advanced-variables和nested之前引入
     'postcss-mixins': {},
     'postcss-advanced-variables': {},
     'postcss-nested': {},
+    'postcss-color-function': {}
     // 'cssnano': {}
   }
 }
